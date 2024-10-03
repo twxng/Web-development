f4dee794d4502771b58c64243f2c6a4aaa372c6b
XXNXX <savethatsh2xan@gmail.com>
2024-10-03 18:31:14 +0300

    update

--- lr2-hooks/vite.config.js
+++ lr2-hooks/vite.config.js
@@ -3,6 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	base: '/Web-development/',
+	plugins: [react()],
+	base: '/Web-development/', 
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		sourcemap: process.env.NODE_ENV !== 'production',
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+					'ui-vendor': ['flowbite-react'],
+				},
+			},
+		},
+	},
+	optimizeDeps: {
+		include: ['react', 'react-dom', 'react-router-dom', 'flowbite-react'],
+	},
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
 })
